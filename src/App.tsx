23c8d0856bebd7628613b34400184e99bdc746ae
a-knaqq <aa@aa>
2024-09-19 10:21:13 -0400

    StorageImage push

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import { uploadData } from 'aws-amplify/storage';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
+"use client"
+
+
+
 
 function App() {
-  const [file, setFile] = React.useState();
-
-  const handleChange = (event: any) => {
-    setFile(event.target.files[0]);
-  };
-
-  return (
-    <div>
-      <input type="file" onChange={handleChange} />
-        <button
-          onClick={() =>
-            uploadData({
-              path: `photos/${file.name}`,
-              data: file,
-          })
-        }
-      >
-        Upload
-      </button>
-    </div>
-  );
+  return <StorageImage alt='image' path="s3://amplify-d3t1hov1f4zii-mai-journalphotosbucket67b0e-pib8dehnr3in/photos/IMG_4614.jpeg"/>
 }
 export default App;
